59578ad169edbc1cefcc74cc0cf1ba8885ed66aa
khalid21456 <khalidedaoudi8@gmail.com>
2025-09-03 19:41:25 +0100

    test container for postgreSql database

--- server/src/test/java/com/eshop/e_shop/EShopApplicationTests.java
+++ server/src/test/java/com/eshop/e_shop/EShopApplicationTests.java
@@ -4,10 +4,21 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Testcontainers
 class EShopApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
+
+	 @Container
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:15-alpine");
+
+	@DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+    }
 
 }
